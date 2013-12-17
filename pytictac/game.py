ab6705781a36c95b644daaeed9a9335f0a855af4
devourer09 <matthewsburson@gmail.com>
2013-12-17 10:45:16 -0600

    Init commit

---
+++ pytictac/game.py
@@ -0,0 +1,34 @@
+#from __future__ import print_function
+
+import random
+import time
+
+class Board:
+    def __init__(self):
+        self.location_values = ('. ', 'x ', 'o ')
+        self.location = []
+        random.seed(int(time.time()))
+        for i in range(3):
+            self.location.append([])
+            for j in range(3):
+                self.location[i].append('x')
+
+    def print_board(self, board=''):
+        print()
+        if board == '':
+            board = self.location
+
+        for i in board:
+            for j in i:
+                print(j, end='')
+            print()
+
+    def populate(self):
+        for i, ii in enumerate(self.location):
+            for j, jj in enumerate(ii):
+                self.location[i][j] = self.location_values[random.randint(0, 2)]
+
+    def rotate(self):
+        #tmp = copy.deepcopy(self.location)
+
+        self.location = zip(*self.location[::-1])
---
+++ pytictac/pytictac.py
@@ -0,0 +1,10 @@
+#from __future__ import print_function
+
+import game
+
+if __name__ == "__main__":
+    b = game.Board()
+    b.populate()
+    for i in range(4):
+        b.print_board()
+        b.rotate()
